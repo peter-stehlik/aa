5aebd5abaa02be92c761abcd1ba8d24e779868c8
Peter Stehlik <peter@inovative.sk>
2021-12-22 22:44:49 +0100

    finish HTML header

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,4 +1,7 @@
 let AA = {
+	$menu: document.querySelector(".nav-wrap"),
+	$subNavServices: document.querySelector(".sub-nav-services"),
+
 	toggleLangs: () => {
 		document.querySelector(".js-lang-toggler").addEventListener("click", (event) => {
 			document.querySelector(".js-langs-wrap").classList.toggle("is-opened");
@@ -7,54 +10,99 @@ let AA = {
 	toggleSubMenuService: () => {
 		let $serviceSubMenu = document.querySelector(".sub-menu-service a");
 		$serviceSubMenu.addEventListener("click", (event) => {
-			let $el = document.querySelector(".sub-nav-services");
-			let toggle = () => {
-				$el.classList.toggle("-z-10");
-				$el.classList.toggle("opacity-0");
-				$el.classList.toggle("-translate-y-4");
-			};
+			let $el = AA.$subNavServices;
 			
-			if( $serviceSubMenu.classList.contains("is-active") ){
-				toggle();
-
-				setTimeout(function(){
-					$el.classList.toggle("hidden");
-				}, 200); 
-			} else {
-				$el.classList.toggle("hidden");
-				setTimeout(function(){
+			if( window.innerWidth > 1024 ){
+				let toggle = () => {
+					$el.classList.toggle("is-active");
+				};
+				
+				if( $serviceSubMenu.classList.contains("is-active") ){
 					toggle();
-				}, 100);
+					
+					setTimeout(function(){
+						$el.classList.toggle("lg:hidden");
+					}, 200); 
+				} else {
+					$el.classList.toggle("lg:hidden");
+					setTimeout(function(){
+						toggle();
+					}, 100);
+				}
+				
+				$serviceSubMenu.classList.toggle("is-active");
+			} else { // mobile
+				AA.helperOpenMobSubNav();
 			}
-			
-			$serviceSubMenu.classList.toggle("is-active");
 		});
 	},
 	tidyHeader: () => {
 		let y = window.scrollY;
 		let $header = document.querySelector("header");
 
-		if( y > 50 ){
-			$header.classList.remove("lg:py-7");
-			$header.classList.remove("py-2");
-			$header.classList.add("lg:py-1");
-			$header.classList.add("py-0");
-			$header.classList.add("shadow-grey-500");
-			$header.classList.add("shadow-lg");
+		if( y > 50 ){			
+			$header.classList.remove("lg:py-7", "py-2");
+			$header.classList.add("lg:py-1", "py-0", "shadow-grey-500", "shadow-lg");
 		} else {
-			$header.classList.remove("lg:py-1");
-			$header.classList.remove("py-0");
-			$header.classList.remove("shadow-grey-500");
-			$header.classList.remove("shadow-lg");
-			$header.classList.add("lg:py-7");
-			$header.classList.add("py-2");
+			$header.classList.remove("lg:py-1", "py-0", "shadow-grey-500", "shadow-lg");
+			$header.classList.add("lg:py-7", "py-2");
 		}
+	},
+	toggleMobNav: () => {
+		document.querySelector(".js-nav-toggler").addEventListener("click", () => {
+			let $hamburger = document.querySelector("#hamburger");
+
+			if( $hamburger.classList.contains("is-opened") ){
+				AA.helperCloseMobNav();
+			} else {
+				AA.helperOpenMobNav();
+			}
+
+			$hamburger.classList.toggle("is-opened");
+		});
+		
+		document.querySelector(".js-back-mob-nav").addEventListener("click", () => {
+			AA.helperOpenMobNav();
+		});
+	},
+	helperCloseMobNav: () => {
+		AA.$menu.classList.remove("is-ready", "is-opened");
+
+		AA.$subNavServices.classList.remove("is-ready", "is-opened");
+	},
+	helperOpenMobNav: () => {
+		AA.$menu.classList.add("is-opened");
+		AA.$menu.classList.remove("is-ready");
+
+		AA.$subNavServices.classList.add("is-ready");
+		AA.$subNavServices.classList.remove("is-opened");
+	},
+	helperOpenMobSubNav: () => {
+		AA.$menu.classList.remove("is-opened");
+		AA.$menu.classList.add("is-ready");
+
+		AA.$subNavServices.classList.remove("is-ready");
+		AA.$subNavServices.classList.add("is-opened");
+	},
+	clearHeaderOnResize: () => {
+		setTimeout(function(){
+			document.querySelector(".sub-menu-service a").classList.remove("is-active");
+			document.querySelector("#hamburger").classList.remove("is-opened");
+			
+			AA.$subNavServices.classList.add("lg:hidden");
+			AA.$subNavServices.classList.remove("is-active");
+			
+			AA.helperCloseMobNav();
+		}, 700);
 	},
 }
 
 AA.toggleLangs();
 AA.toggleSubMenuService();
+AA.toggleMobNav();
 
 document.addEventListener('scroll', function(e) {
 	AA.tidyHeader();
 });
+
+window.addEventListener('resize', AA.clearHeaderOnResize);
